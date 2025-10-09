851a7e751c95211de6601b16ae24d26fc51fecb7
Luiz Chaves <lucachaves@gmail.com>
2025-10-09 08:57:48 -0300

    classroom: Add JavaScript file demonstrating variable declarations with var, let, and const

---
+++ classroom/ecma/variable.js
@@ -0,0 +1,20 @@
+// var, let, const
+let nome = 'Luca';
+const pi = 3.14;
+var idade = 25;
+
+// CONST
+// 'const' declarations must be initialized.
+// const number;
+
+const number = 10;
+
+// TypeError: Assignment to constant variable.
+// number = 100;
+
+// Cannot redeclare block-scoped variable 'number'.
+// const number = 100;
+
+// LET
+let numero;
+numero = 100;
